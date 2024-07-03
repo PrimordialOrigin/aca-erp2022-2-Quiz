6c844d69de9fdc29670a6bd618db95171262bfd7
Primordial <RamerafheKaone@protonmail.com>
2024-07-03 14:56:24 +0200

    should more data

--- src/assets/quizData.js
+++ src/assets/quizData.js
@@ -4,70 +4,72 @@ export const data = [
         option1:"JavaScript is a scripting language used to make the website interactive",
         option2:"JavaScript is an assembly language used to make the website interactive",
         option3:"JavaScript is a compiled language used to make the website interactive",
-        option4:"None of the mentioned"
+        option4:"None of the mentioned",
+        answer: 1
     },
     {
         question:"Which of the following is correct about JavaScript?",
         option1:"JavaScript is an Object-Based language",
         option2:"JavaScript is Assembly-language",
         option3:"JavaScript is an Object-Oriented language",
-        option4:"JavaScript is a High-level language"
+        option4:"JavaScript is a High-level language",
+        answer: 1
     },
     {
         question:"Among the given statements, which statement defines closures in JavaScript?",
         option1:" JavaScript is a function that is enclosed with references to its inner function scope",
         option2:"JavaScript is a function that is enclosed with references to its lexical environmen",
         option3:"JavaScript is a function that is enclosed with the object to its inner function scope",
-        option4:"None of the mentioned"
+        option4:"None of the mentioned",
+        answer: 2
     },
     {
         question:"Arrays in JavaScript are defined by which of the following statements?",
         option1:"It is an ordered list of values",
         option2:"It is an ordered list of objects",
         option3:"It is an ordered list of stringe",
-        option4:"It is an ordered list of functions"
+        option4:"It is an ordered list of functions",
+        answer: 1
     },
     {
         question:"Which of the following is not javascript data types?",
         option1:"Null type",
         option2:"Undefined type",
         option3:"Number type",
-        option4:"None of the mentioned"
+        option4:"None of the mentioned",
+        answer: 4
     },
     {
         question:"Where is Client-side JavaScript code is embedded within HTML documents?",
         option1:"A URL that uses the special javascript:code",
         option2:"A URL that uses the special javascript:protocol",
         option3:"A URL that uses the special javascript:encoding",
-        option4:"A URL that uses the special javascript:stack"
+        option4:"A URL that uses the special javascript:stack",
+        answer: 2
     },
     {
         question:"Which of the following object is the main entry point to all client-side JavaScript features and APIs?",
         option1:"Position",
         option2:"Window",
         option3:"Standard",
-        option4:"Location"
+        option4:"Location",
+        answer: 2
     },
     {
         question:"Which of the following can be used to call a JavaScript Code Snippet?",
         option1:"Function/Method",
         option2:"Preprocessor",
         option3:"Triggering Event",
-        option4:"RMI"
+        option4:"RMI",
+        answer: 1
     },
     {
         question:"Which of the following explains correctly what happens when a JavaScript program is developed on a Unix Machine?",
         option1:"will work perfectly well on a Windows Machine",
         option2:"will be displayed as JavaScript text on the browser",
         option3:"will throw errors and exceptions",
-        option4:"must be restricted to a Unix Machine only"
-    },
-    {
-        question:"What is JavaScript?",
-        option1:"JavaScript is a scripting language used to make the website interactive",
-        option2:"JavaScript is an assembly language used to make the website interactive",
-        option3:"JavaScript is a compiled language used to make the website interactive",
-        option4:"None of the mentioned"
+        option4:"must be restricted to a Unix Machine only",
+        answer: 1
     },
     {
         question:"What is JavaScript?",
