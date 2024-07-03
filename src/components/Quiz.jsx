83ff5accd26e9a80c0d5f7ddddddc5aef07bb5c4
Primordial <RamerafheKaone@protonmail.com>
2024-07-03 15:01:18 +0200

    update

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -7,18 +7,31 @@ function Quiz() {
     let [questionIndex, setQuestionIndex] = useState(0);
     let [question, setQuestion] = useState(data[questionIndex]);
 
+    const checkMyAnswer = (element, answer) => {
+        if(question.answer===answer) {
+            element.target.classList.add("correct");
+        }
+        else{
+            element.target.classList.add("wrong")
+        }
+    }
+    const next = () => {
+        setQuestionIndex(++questionIndex);
+        setQuestion(data[questionIndex]);
+    }
+
     return(
         <div className='container'>
             <h1>Javascript Quiz</h1>
             <div className="itemsContainer">
                 <h3>{questionIndex+1}. {question.question}</h3>
                 <ul>
-                    <li>{question.option1}</li>
-                    <li>{question.option2}</li>
-                    <li>{question.option3}</li>
-                    <li>{question.option4}</li>
+                    <li onClick={(element)=>{checkMyAnswer(element,1)}}>{question.option1}</li>
+                    <li onClick={(element)=>{checkMyAnswer(element,2)}}>{question.option2}</li>
+                    <li onClick={(element)=>{checkMyAnswer(element,3)}}>{question.option3}</li>
+                    <li onClick={(element)=>{checkMyAnswer(element,4)}}>{question.option4}</li>
                 </ul>
-                <button>Next question</button>
+                <button onClick={next}>Next question</button>
             </div>
         </div>
     )
