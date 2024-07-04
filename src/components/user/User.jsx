e138f78ec1c66e25a4af5cae548b50943b6682f1
Primordial <RamerafheKaone@protonmail.com>
2024-07-04 17:09:24 +0200

    Username entry

---
+++ src/components/user/User.jsx
@@ -0,0 +1,29 @@
+import React, {useContext, useState, useEffect} from "react";
+import { AppContext } from '../Context';
+
+function User() {
+
+    let {setQuizState} = useContext(AppContext);
+    let [username, setUsername] = useState("");
+
+    function getName(event){
+        setUsername(event.target.value);
+    }
+
+    useEffect(() =>{
+        localStorage.setItem("Name",JSON.stringify(username))
+    }, [username]);
+    
+     const next = () => {setQuizState("container")}
+  return (
+    <div className="userForm">
+        <h1>WELCOME PARTICIPATION</h1>
+        <div className="item">
+            <input type="text" value={username} placeholder="Enter your name" onChange={getName}/>
+            <button onClick={next}>Enter</button>
+        </div>
+    </div>
+  )
+}
+
+export default User
