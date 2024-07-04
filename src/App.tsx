b7730e9111bb3f3f2b5050113b6b3a25f840bfe6
Primordial <RamerafheKaone@protonmail.com>
2024-07-04 02:51:20 +0200

    UPDATE
    This reverts commit 760e8490e851f5d3825be8fa2eb3b4b0e7a59fc0.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, {useState, useContext} from 'react';
 import Quiz from './components/Quiz'
+import ScoreScreen from './components/ScoreScreen';
+import { AppContext } from './components/Context';
+
 
 function App() {
+  let [quizState, setQuizState] = useState("container");
+  let [score, setScore] = useState(0);
+
   return (
     <div className="App">
-      <Quiz />
+      <AppContext.Provider value={{quizState, setQuizState, score, setScore}}>
+        {quizState === "container" && <Quiz/>}
+        {quizState === "endGame" && <ScoreScreen/>}
+      </AppContext.Provider> 
     </div>
   );
 }
