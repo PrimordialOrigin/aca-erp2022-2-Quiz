9e63a9f4f3393be1472979d375014e5473d1be18
Primordial <RamerafheKaone@protonmail.com>
2024-07-04 11:40:15 +0200

    Revert "changed the folder structure"
    This reverts commit 6aad503bbb858e4b89b73f1e64e430b6e5ca68a7.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, {useState, useContext} from 'react';
-import Quiz from './components/quiz/Quiz'
-import ScoreScreen from './components/scores/ScoreScreen';
+import Quiz from './components/Quiz'
+import ScoreScreen from './components/ScoreScreen';
 import { AppContext } from './components/Context';
 
 
