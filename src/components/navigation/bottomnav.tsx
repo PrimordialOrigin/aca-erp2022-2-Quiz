ff542de929980dff04d76bdce0a2c0f8416dae44
ragmer09toske <retsepile.raymondshao@gmail.com>
2024-07-05 12:23:17 +0200

    Responsivity on Mobile devises, also completed the issue on red color next

--- src/components/navigation/bottomnav.tsx
+++ src/components/navigation/bottomnav.tsx
@@ -7,6 +7,7 @@ import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import { AppContext } from '../Context';
 
 import LibraryBooksIcon from '@mui/icons-material/LibraryBooks';
+import { MoreVert } from '@mui/icons-material';
 export default function BottomNav() {
   const [value, setValue] = React.useState(0);
   let {setQuizState} = React.useContext(AppContext);
@@ -17,20 +18,24 @@ export default function BottomNav() {
   }
 
   return (
-    <Box sx={{ width: 800, display:"flex", justifyContent: "center" }}>
-      <BottomNavigation
-        showLabels
-        value={value}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
-        sx={{ width: 785 }}
-      >
-        <BottomNavigationAction label="Back" onClick={handleBack} icon={<ArrowBackIosIcon />} />
-        <BottomNavigationAction label="Back" onClick={handleBack} icon={<ArrowBackIosIcon />} />
-        <BottomNavigationAction label="Material" icon={<LibraryBooksIcon />} />
-        <BottomNavigationAction label={!name ? "user" : name } icon={<AccountCircleIcon />} />
-      </BottomNavigation>
-    </Box>
+    <div className='lg:w-[800px]'>
+      <Box sx={{ display:"flex", justifyContent: "center" }}>
+        <div className='lg:w-[785px] flex justify-center '>
+          <BottomNavigation
+            showLabels
+            value={value}
+            onChange={(event, newValue) => {
+              setValue(newValue);
+            }}
+            sx={{ width: "90%", padding: 4, justifyContent: "center" }}
+          >
+            <BottomNavigationAction label="More" onClick={handleBack} icon={<MoreVert />} />
+            <BottomNavigationAction label="Back" onClick={handleBack} icon={<ArrowBackIosIcon />} />
+            <BottomNavigationAction label="Material" icon={<LibraryBooksIcon />} />
+            <BottomNavigationAction label={!name ? "user" : name } icon={<AccountCircleIcon />} />
+          </BottomNavigation>
+        </div>
+      </Box>
+    </div>
   );
 }
--- src/components/quiz/Quiz.jsx
+++ src/components/quiz/Quiz.jsx
@@ -1,110 +1,113 @@
-//DONT FORGET TO RESEARCH MORE ABOUT LOCALSTORAGE...COULD POSSIBLY USE IT TO SAVE THE QUIZ SCORE -0750HRS
-
-import React, {useRef, useState, useContext} from 'react';
-import {data} from '../../assets/quizData';
+import React, { useRef, useState, useContext } from 'react';
+import { data } from '../../assets/quizData';
 import { AppContext } from '../Context';
 import './Quiz.css';
 import { Button, Card, Typography } from '@mui/material';
 
 function Quiz() {
+    const { setQuizState, score, setScore } = useContext(AppContext);
+    const [questionIndex, setQuestionIndex] = useState(0);
+    const [closed, setClosed] = useState(false);
 
-    let {setQuizState, score, setScore} = useContext(AppContext);
-    let [questionIndex, setQuestionIndex] = useState(0);
-    let [question, setQuestion] = useState(data[questionIndex]);
-    let [closed, setClosed] = useState(false);
-    
+    const option1 = useRef(null);
+    const option2 = useRef(null);
+    const option3 = useRef(null);
+    const option4 = useRef(null);
 
-    let option1 = useRef(null);
-    let option2 = useRef(null);
-    let option3 = useRef(null);
-    let option4 = useRef(null);
+    const myOptions = [option1, option2, option3, option4];
 
-    let myOptions = [option1, option2, option3, option4];
+    const question = data[questionIndex];
 
-    //checks the answer 
+    // Checks the answer
     const checkMyAnswer = (element, answer) => {
-        if(closed === false){
-            if(question.answer===answer) {
-                element.target.classList.add("correct");
-                setClosed(true);
+        if (!closed) {
+            if (question.answer === answer) {
+                element.target.classList.add('correct');
                 setScore(score + 1);
+                // Remove the 'wrong' class after 2 seconds
+                setTimeout(() => {
+                    element.target.classList.remove('correct');
+                }, 3000);
+            } else {
+                element.target.classList.add('wrong');
+                myOptions[question.answer - 1].current.classList.add('correct');
+                 // Remove the 'wrong' class after 2 seconds
+                 setTimeout(() => {
+                    element.target.classList.remove('wrong');
+                }, 2000);
             }
-            else{
-                element.target.classList.add("wrong");
-                setClosed(true);
-                myOptions[question.answer-1].current.classList.add("correct");
-            }
+            setClosed(true);
         }
+    };
 
-    }
-    //navigates to the next question
+    // Resets the color classes
+    const resetColors = () => {
+        myOptions.forEach(option => {
+            if (option.current) {
+                option.current.classList.remove('wrong');
+                option.current.classList.remove('correct');
+            }
+        });
+    };
+
+    // Navigates to the next question
     const next = () => {
-        if(closed === true){
-            setQuestionIndex(++questionIndex);
-            setQuestion(data[questionIndex]);
-            setClosed(false);
-            //Reset the color scheme
-            myOptions.map((option) => {
-                option.current.classList.remove("wrong");
-                option.current.classList.remove("correct");
-                return null;
-            })
+        if (closed) {
+            resetColors();
+            if (questionIndex < data.length - 1) {
+                setQuestionIndex(prevIndex => prevIndex + 1);
+                setClosed(false);
+            }
         }
-    }
+    };
 
+    // Ends the quiz
     const done = () => {
-        if(closed === true){
-            setQuestionIndex(++questionIndex);
-            setQuestion(data[questionIndex]);
-            setQuizState("endGame");
+        if (closed) {
+            resetColors();
+            setQuizState('endGame');
         }
-        
-    }
+    };
 
-    return(
-    <div className="" style={{padding: 10}}>
-        <Card>
-            <Typography variant="h2" component="div">
-                Quiz
-            </Typography>
-            <div style={{padding: 25}}>
-                <Typography variant="h5" component="div">
-                    {questionIndex+1}. {question.question}
+    return (
+        <div style={{ padding: 10 }}>
+            <Card>
+                <Typography variant="h2" component="div">
+                    Quiz
                 </Typography>
-            </div>
-            <h3></h3>
-            <div className="itemsContainer">
-                <ul>
-                    <li ref={option1} onClick={(element)=>{checkMyAnswer(element,1)}}>
-                        <Typography variant="body2">
-                            {question.option1}
-                        </Typography>
-                    </li>
-                    <li ref={option2} onClick={(element)=>{checkMyAnswer(element,2)}}>
-                        <Typography variant="body2">
-                            {question.option2}
-                        </Typography>
-                    </li>
-                    <li ref={option3} onClick={(element)=>{checkMyAnswer(element,3)}}>
-                        <Typography variant="body2">
-                            {question.option3}
-                        </Typography>
-                    </li>
-                    <li ref={option4} onClick={(element)=>{checkMyAnswer(element,4)}}>
-                        <Typography variant="body2">
-                            {question.option1}
-                        </Typography>
-                    </li>
-                </ul>
-                {questionIndex === data.length - 1 ? (
-                    <Button onClick={done} variant="contained" style={{paddingLeft: 20, paddingRight:20, paddingTop: 5}}>Last question</Button>
-                ):(
-                    <Button onClick={next} sx={{pl:10, pr:10}} style={{paddingLeft: 20, paddingRight:20, paddingTop: 5}} variant="contained">Next question</Button>
-                )}
-            </div>  
-        </Card>
-    </div>  
-    )
+                <div style={{ padding: 25 }}>
+                    <Typography variant="h5" component="div">
+                        {questionIndex + 1}. {question.question}
+                    </Typography>
+                </div>
+                <div className="itemsContainer">
+                    <ul>
+                        <li ref={option1} onClick={(element) => checkMyAnswer(element, 1)}>
+                            <Typography variant="body2">{question.option1}</Typography>
+                        </li>
+                        <li ref={option2} onClick={(element) => checkMyAnswer(element, 2)}>
+                            <Typography variant="body2">{question.option2}</Typography>
+                        </li>
+                        <li ref={option3} onClick={(element) => checkMyAnswer(element, 3)}>
+                            <Typography variant="body2">{question.option3}</Typography>
+                        </li>
+                        <li ref={option4} onClick={(element) => checkMyAnswer(element, 4)}>
+                            <Typography variant="body2">{question.option4}</Typography>
+                        </li>
+                    </ul>
+                    {questionIndex === data.length - 1 ? (
+                        <Button onClick={done} variant="contained" style={{ paddingLeft: 20, paddingRight: 20, paddingTop: 5 }}>
+                            Last question
+                        </Button>
+                    ) : (
+                        <Button onClick={next} sx={{ pl: 10, pr: 10 }} style={{ paddingLeft: 20, paddingRight: 20, paddingTop: 5 }} variant="contained">
+                            Next question
+                        </Button>
+                    )}
+                </div>
+            </Card>
+        </div>
+    );
 }
 
 export default Quiz;
